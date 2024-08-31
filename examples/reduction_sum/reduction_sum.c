311fe238f4f4208b864daca7d0e3a9a92e501e5e
Ryabykin Vladislav <ryabykin.vlad@mail.ru>
2024-08-31 15:32:43 +0000

    make common reduction function in examples folder

--- examples/reduction_sum/reduction_sum.c
+++ examples/reduction_sum/reduction_sum.c
@@ -14,18 +14,105 @@
 #include <unistd.h>
 #include <stdarg.h>
 #include <abt.h>
+#include <string.h>
 
 #define DEFAULT_NUM_XSTREAMS 2
 #define DEFAULT_NUM_THREADS 8
 
-typedef struct {
-    int tid;
-} thread_arg_t;
+#define NUM_ELEMS 1024
 
-void hello_world(void *arg)
-{
-    int tid = ((thread_arg_t *)arg)->tid;
-    printf("Hello world! (thread = %d)\n", tid);
+
+typedef struct {
+    ABT_xstream *xstreams;
+    int num_xstreams;
+    ABT_pool *pools;
+    int num_pools;
+    ABT_thread *threads;
+    int num_threads;
+} reduction_context_t;
+
+
+typedef struct {
+    void *array;                         /* array, on which reduction will be performed */
+    size_t num_elems;                    /* number of elements to perform reduction on */
+    size_t elem_size;                    /* size of a single element */
+    void* default_reduction_value;       /* 0 for sum, 1 for multiplication, etc. */
+    void *result;                        /* where to store the result of reduction */
+    void (*reduce_func)(void *, void *); /* provided reduction function on 2 elements */
+    ABT_mutex mutex;                     /* mutex to perform final (among different threads) reduction */
+} reduction_args_t;
+
+
+void reduction_thread(void *arg) {
+    reduction_args_t *reduction_args = (reduction_args_t *)arg;
+    size_t num_elems = reduction_args->num_elems;
+    size_t elem_size = reduction_args->elem_size;
+    char *array = (char *)reduction_args->array;
+    
+    // Initialize local result to default value of a reduciton
+    void *local_result = (void *)malloc(elem_size);
+    memcpy(local_result, reduction_args->default_reduction_value, elem_size);
+
+    for (size_t i = 0; i < num_elems; ++i) {
+        reduction_args->reduce_func(local_result, array + i * elem_size);
+    }
+
+    ABT_mutex_lock(reduction_args->mutex);
+    reduction_args->reduce_func(reduction_args->result, local_result);
+    ABT_mutex_unlock(reduction_args->mutex);
+
+    free(local_result);
+}
+
+
+void reduce_common(
+    reduction_context_t *reduction_context,
+    void *array,
+    size_t num_elems,
+    size_t elem_size,
+    void *default_reduction_value,
+    void (*reduce_func)(void *, void *),
+    void *result
+) {
+    int num_threads = reduction_context->num_threads;
+    size_t elems_per_thread = num_elems / num_threads;
+    reduction_args_t *thread_args = 
+        (reduction_args_t *)malloc(sizeof(reduction_args_t) * num_threads);
+    ABT_mutex mutex;
+    ABT_mutex_create(&mutex);
+
+    for (int i = 0; i < num_threads; ++i) {
+        thread_args[i].array = (char *)array + i * elems_per_thread * elem_size;
+        thread_args[i].num_elems = (i == num_threads - 1) ? (num_elems - i * elems_per_thread) : elems_per_thread;
+        thread_args[i].elem_size = elem_size;
+        thread_args[i].default_reduction_value = default_reduction_value;
+        thread_args[i].result = result;
+        thread_args[i].reduce_func = reduce_func;
+        thread_args[i].mutex = mutex;
+    }
+
+    for (int i = 0; i < num_threads; ++i) {
+        int pool_id = i % reduction_context->num_xstreams;
+        ABT_thread_create(
+            reduction_context->pools[pool_id],
+            reduction_thread,
+            &thread_args[i],
+            ABT_THREAD_ATTR_NULL,
+            &(reduction_context->threads[i])
+        );
+
+    }
+
+    for (int i = 0; i < num_threads; ++i) {
+        ABT_thread_join(reduction_context->threads[i]);
+    }
+
+    ABT_mutex_free(&mutex);
+    free(thread_args);
+}
+
+void sum_reduction_func(void *a, void *b) {
+    *((int *)a) += *((int *)b);
 }
 
 int main(int argc, char **argv)
@@ -47,7 +134,7 @@ int main(int argc, char **argv)
                 break;
             case 'h':
             default:
-                printf("Usage: ./hello_world [-e NUM_XSTREAMS] "
+                printf("Usage: ./reduction_sum [-e NUM_XSTREAMS] "
                        "[-n NUM_THREADS]\n");
                 return -1;
         }
@@ -60,11 +147,11 @@ int main(int argc, char **argv)
     /* Allocate memory. */
     ABT_xstream *xstreams =
         (ABT_xstream *)malloc(sizeof(ABT_xstream) * num_xstreams);
-    ABT_pool *pools = (ABT_pool *)malloc(sizeof(ABT_pool) * num_xstreams);
+    int num_pools = num_xstreams;
+    ABT_pool *pools = (ABT_pool *)malloc(sizeof(ABT_pool) * num_pools);
     ABT_thread *threads =
         (ABT_thread *)malloc(sizeof(ABT_thread) * num_threads);
-    thread_arg_t *thread_args =
-        (thread_arg_t *)malloc(sizeof(thread_arg_t) * num_threads);
+    
 
     /* Initialize Argobots. */
     ABT_init(argc, argv);
@@ -82,16 +169,37 @@ int main(int argc, char **argv)
         ABT_xstream_get_main_pools(xstreams[i], 1, &pools[i]);
     }
 
-    /* Create ULTs. */
-    for (i = 0; i < num_threads; i++) {
-        int pool_id = i % num_xstreams;
-        thread_args[i].tid = i;
-        ABT_thread_create(pools[pool_id], hello_world, &thread_args[i],
-                          ABT_THREAD_ATTR_NULL, &threads[i]);
+    /* Create array to reduce. */
+    size_t elem_size = sizeof(int);
+    int *array = (int *)malloc(sizeof(int) * NUM_ELEMS);
+    for (size_t idx = 0; idx < NUM_ELEMS; ++idx) {
+      array[idx] = idx;
     }
+
+    int default_reduction_value = 0;
+    int result = 0;
+    reduction_context_t reduction_context = {
+        .xstreams = xstreams,
+        .num_xstreams = num_xstreams,
+        .pools = pools,
+        .num_pools = num_pools,
+        .threads = threads,
+        .num_threads = num_threads,
+    };
+
+    reduce_common(
+        &reduction_context,
+        array,
+        NUM_ELEMS,
+        elem_size,
+        &default_reduction_value,
+        sum_reduction_func,
+        &result
+    );
 
     /* Join and free ULTs. */
     for (i = 0; i < num_threads; i++) {
+        // ABT_thread_join(threads[i]);
         ABT_thread_free(&threads[i]);
     }
 
@@ -108,7 +216,10 @@ int main(int argc, char **argv)
     free(xstreams);
     free(pools);
     free(threads);
-    free(thread_args);
+
+    printf("Reduction result=%d\n", result);
+
+    free(array);
 
     return 0;
 }
